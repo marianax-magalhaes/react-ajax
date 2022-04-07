6da7a6701ba7fdd1106dfbbfbc26e8b9e67581b5
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-04-07 15:53:32 -0300

    metodo delete

--- src/components/Usuarios.js
+++ src/components/Usuarios.js
@@ -26,10 +26,20 @@ class Usuarios extends Component {
   removerUsuario(usuario) {
 
     if (window.confirm(`Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`)) {
-      let usuarios = this.state.usuarios
-      usuarios = usuarios.filter(x => x.id !== usuario.id)
 
-      this.setState({ usuarios: usuarios })
+        fetch(`https://reqres.in/api/users/${usuario.id}`,{
+            method: 'DELETE'
+        })
+        .then(resposta=>{
+            console.log(resposta)
+
+            if(resposta.ok){
+                let usuarios = this.state.usuarios
+                usuarios = usuarios.filter(x => x.id !== usuario.id)
+                this.setState({ usuarios: usuarios })
+            }
+        })
+
     }
 
   }
